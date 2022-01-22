719570ab42b0ab6a2e227713bc96b2d716540e65
juan_ <juan_@DESKTOP-M5ASF7R>
2022-01-21 21:44:50 -0500

    Agregar archivos de proyecto.

---
+++ Singleton/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonPro A1 = SingletonPro.ObtenerInstancia();
+            SingletonPro A2 = SingletonPro.ObtenerInstancia();
+
+            string resultado;
+
+            if (A1 == A2)
+            {
+                resultado = "Ambas instancias son la misma.";
+            }
+            else
+            {
+                resultado = "Son diferentes instancias.";
+            }
+
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/SingletonPro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class SingletonPro
+      {
+        private static SingletonPro instancia;
+
+        private SingletonPro() { }
+
+        public static SingletonPro ObtenerInstancia()
+        {
+            if (instancia == null)
+                instancia = new SingletonPro();
+
+            return instancia;
+        }
+    }
+}
